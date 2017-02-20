f7e36ac31dd42f6a4b68e0cb910d2f7a7db43921
xerfgydt <rune1005@gmail.com>
2017-02-20 11:33:31 +0100

    Add files via upload

---
+++ Rabbit.java
@@ -0,0 +1,44 @@
+public class Rabbit {
+
+    private String name;
+    private int xPosition;
+    private int yPosition;
+
+    public Rabbit(String name, int xPosition, int yPosition) {
+        this.name = name;
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+    }
+    
+    public void setNameRabbit(String name) {
+        System.out.println("Rabbit");
+    }
+    public void moveUp(){
+        this.yPosition = yPosition + 1 ;
+
+    }
+    public void moveDown(){
+        this.yPosition = yPosition - 1 ;
+
+    }
+    public void moveRight(){
+        this.xPosition = xPosition + 1 ;
+
+    }
+    public void moveLeft(){
+        this.xPosition = xPosition - 1 ;
+
+    }
+    
+    public void tellPosition(){
+        System.out.println("Jeg er Rabbit. Jeg er her: " + xPosition +"," + yPosition);
+    }
+    
+    //public void mercy(String) {
+        //System.out.println("ask for mercy")
+    //}
+}
+
+
+
+
